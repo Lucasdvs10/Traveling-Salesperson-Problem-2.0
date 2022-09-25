df2d779d84a3e80b605f0825083c2dd5cbc696f8
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-09-25 09:18:45 -0300

    Ants are enable to go to hard coded city

--- Assets/Scripts/Core Scripts/Entities/Ant.cs
+++ Assets/Scripts/Core Scripts/Entities/Ant.cs
@@ -6,5 +6,42 @@ namespace Entities {
         private City _currentCity;
         private HashSet<City> _visitedCity;
         private HashSet<Path> _visitedPaths;
+
+
+        public void GoToNextCity() {
+            foreach (var path in CurrentCity.PossiblePaths) {
+                
+                //todo: Fazer os cálculos de probabilidade de escolher este caminho
+                
+                //Calcular um numero e ver se este caminho foi escolhido ou não
+                
+                var choseThisPath = true; //Variável mockada para fins de teste
+                
+                if (choseThisPath) {
+                    _currentCity = path.CitiesPath[1];
+                    return;
+                }
+            }
+        }
+        
+        public void ReturnToInitialCity() {
+            _currentCity = _initialCity;
+        }
+        
+        
+        public Ant(City initialCity) {
+            _initialCity = initialCity;
+            _currentCity = _initialCity;
+        }
+
+        public City InitialCity => _initialCity;
+
+        public City CurrentCity => _currentCity;
+
+        public HashSet<City> VisitedCity => _visitedCity;
+
+        public HashSet<Path> VisitedPaths => _visitedPaths;
+
+
     }
 }
--- Assets/Scripts/Tests/Ant_Should.cs
+++ Assets/Scripts/Tests/Ant_Should.cs
@@ -1,11 +1,42 @@
+using System.Collections.Generic;
+using Entities;
 using NUnit.Framework;
-using Assert = UnityEngine.Assertions.Assert;
+using UnityEngine;
 
 namespace Tests {
-    public class Kappa {
-        [Test]
-        public void Kappak() {
-            Assert.AreEqual(1,1);
+    public class Ant_Should {
+        private Ant _ant;
+        
+        [SetUp]
+        public void SetupTests() {
+            var cityA = new City(Vector2.one);
+            _ant = new Ant(cityA);
         }
+        
+        [Test]
+        public void Change_Current_City_To_Initial_City() {
+            _ant.ReturnToInitialCity();
+            
+            Assert.AreEqual(_ant.InitialCity, _ant.CurrentCity);
+        }
+
+        [Test]
+        public void Go_To_Another_City() {
+            var cityA = new City(Vector2.one);
+            var cityB = new City(Vector2.one * 10);
+
+            _ant = new Ant(cityA);
+            
+            Path mockPath = new Path(cityA, cityB, 10f) ;
+            
+            cityA.AddPathToSet(mockPath);
+            cityB.AddPathToSet(mockPath);
+
+            _ant.GoToNextCity();
+            
+            Assert.AreNotEqual(cityA, _ant.CurrentCity);
+
+        }
+        
     }
 }
